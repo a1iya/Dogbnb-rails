0de6af82e98d17cae358349291fba53c9a8e7840
a1iya <aliyusha.gizatullina@gmail.com>
2019-02-01 01:01:39 +0100

    version finale

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  city = City.create(name: Faker::GameOfThrones.city)
+end
+
+30.times do
+  dog = Dog.create(name: Faker::Pokemon.name,
+                   city_id: Faker::Number.between(1, 5))
+end
+
+10.times do
+  dogsitter = Dogsitter.create(name: Faker::Name.first_name,
+                               city_id: Faker::Number.between(1, 5))
+end
+
+30.times do
+  stroll = Stroll.create(date: Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today),
+						dogsitter_id: Faker::Number.between(1, 10),
+                         dog_id: Faker::Number.between(1, 30))
+end
